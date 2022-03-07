b4b2eb0a998c025432d13139a93a79a273818652
JungeunKwon <wow_v_v@naver.com>
2022-03-08 00:33:12 +0900

    [추가] Movie App CSS 적용 전 소스

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 <img src="https://nomadcoders.co/_next/image?url=https%3A%2F%2Fd1telmomo28umc.cloudfront.net%2Fmedia%2Fpublic%2Fthumbnails%2Freact-for-beginners.jpeg&w=1920&q=75" alt="nomad" style="width:300px; height:200px" />
 
 Nomad 무료 강좌 **ReactJS로 영화 웹 서비스 만들기** 를 수강 하면서 React 를 연습 하기 위한 Repo 입니다. 
+
+
+
+----
+
+**[기본 개념]**
+
+React : React JS 는 어플리케이션이 interactive 하도록 만들어 주는 library
+
+React-dom : 모든 React element 들을 HTML Body에 둘 수 있도록 해주는 library 또는 package
+
+JSX : JavaScript를 확장한 문법
+
+Babel : JSX로 적은 코드를 브라우저가 이해할 수 있는 형태로 바꿔준다.
+
+**[state]**
+
--- movie/src/App.js
+++ movie/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    // await를 명시해둔 동작이 끝날때 까지 기다려라는 뜻 getMoives라는 함수가 시간이 좀 걸릴테니 기다려 달라 라고 자바스크립트에게 알린다.
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text"> "Loading..."</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
